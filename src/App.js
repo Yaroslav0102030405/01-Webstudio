d4dc3c33d165f5164c97a1ef2fcf61e5cae59e38
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-10-04 20:53:33 +0300

    update-1

--- src/App.js
+++ src/App.js
@@ -33,7 +33,10 @@ function App() {
 
       <Suspense fallback={<h1>Загрузка...</h1>}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={<Home />}
+          />
           <Route path="/portfolio" element={<Portfolio />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>

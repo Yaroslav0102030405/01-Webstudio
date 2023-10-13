2c54698258be2d4b9dd39bbd5c29e0088831d8b3
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-10-13 12:56:04 +0300

    animation modal and burger-menu

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Dropdown.scss';
+import { CSSTransition } from 'react-transition-group';
 
 import { useTranslation } from 'react-i18next';
 // import { Transition } from 'react-transition-group';
@@ -32,67 +33,74 @@ const Dropdown = () => {
   // render() {
   return (
     // <Transition in={isOpen} timeout={300}>
-      // {state => (
-        <div className="Dropdown">
-          {/* <div className={ Active ? 'Dropdown__active' : 'Dropdown__menu'}> */}
-          <button type="button" className="Dropdown__toggle" onClick={toggle}>
-            {visible ? (
-              <MobileClose className="icon-close" />
-            ) : (
-              <MobileMenu className="icon-menu" />
-            )}
-          </button>
-          {visible && (
-            <div className="Dropdown__menu">
-              <nav className="header-nav">
-                <ul className="Dropdown__top">
-                  <li className="menu__item current">
-                    <Link to="/">
-                      {t('header.home')}
-                      {/* Studio */}
-                    </Link>
-                  </li>
-                  <li className="menu__item">
-                    <Link to="/portfolio">
-                      {t('header.portfolio')}
-                      {/* Portfolio */}
-                    </Link>
-                  </li>
-                  <li className="menu__item">
-                    <a href="!#">
-                      {t('header.contacts')}
-                      {/* Contacts */}
-                    </a>
-                  </li>
-                </ul>
+    // {state => (
+    <div className="Dropdown">
+      {/* <div className={ Active ? 'Dropdown__active' : 'Dropdown__menu'}> */}
+      <button type="button" className="Dropdown__toggle" onClick={toggle}>
+        {visible ? (
+          <MobileClose className="icon-close" />
+        ) : (
+          <MobileMenu className="icon-menu" />
+        )}
+      </button>
+      {/* {visible && ( */}
+        <CSSTransition
+          in={visible}
+          classNames="alert"
+          timeout={500}
+          unmountOnExit
+        >
+          <div className="Dropdown__menu">
+            <nav className="header-nav">
+              <ul className="Dropdown__top">
+                <li className="menu__item current">
+                  <Link to="/">
+                    {t('header.home')}
+                    {/* Studio */}
+                  </Link>
+                </li>
+                <li className="menu__item">
+                  <Link to="/portfolio">
+                    {t('header.portfolio')}
+                    {/* Portfolio */}
+                  </Link>
+                </li>
+                <li className="menu__item">
+                  <a href="!#">
+                    {t('header.contacts')}
+                    {/* Contacts */}
+                  </a>
+                </li>
+              </ul>
 
-                <ul class="Dropdown-contacts">
-                  <li>
-                    <a
-                      className="contacts__link contacts__icon"
-                      href="mailto:info@example.com"
-                    >
-                      <Envelope className="contacts__svg-envelope" />
-                      info@example.com
-                    </a>
-                  </li>
+              <ul class="Dropdown-contacts">
+                <li>
+                  <a
+                    className="contacts__link contacts__icon"
+                    href="mailto:info@example.com"
+                  >
+                    <Envelope className="contacts__svg-envelope" />
+                    info@example.com
+                  </a>
+                </li>
 
-                  <li>
-                    <a
-                      className="contacts__link contacts__icon"
-                      href="tel:+380961111111"
-                    >
-                      <Smartphone className="contacts__svg" />
-                      +38 096 111 11 11
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          )}
-          {/* </div> */}
-        </div>
-      )}
+                <li>
+                  <a
+                    className="contacts__link contacts__icon"
+                    href="tel:+380961111111"
+                  >
+                    <Smartphone className="contacts__svg" />
+                    +38 096 111 11 11
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </CSSTransition>
+      {/* )} */}
+      {/* </div> */}
+    </div>
+  );}
   //   </Transition>
   // );
   // }
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { CSSTransition } from 'react-transition-group';
 import { motion } from 'framer-motion';
 import './Hero.scss';
 import Modal from '../Modal/Modal';
@@ -60,78 +61,87 @@ const Hero = () => {
             {t('hero.btn')}
             {/* Order Service */}
           </motion.button>
-          {showModal && (
-            <Modal onClose={toogleModal}>
-              <form className="form">
-                <button
-                  className="btn__clouse hover form__icon"
-                  type="button"
-                  onClick={toogleModal}
-                >
-                  X
-                </button>
-                <h1 className="form__title">
-                  {t('form.title')}
-                  {/* Leave your details, we will call you back */}
-                </h1>
-                <input
-                  className="form__input"
-                  type="text"
-                  name="name"
-                  id="name"
-                  autocomplete="name"
-                  placeholder={t('form.name')}
-                  required
-                ></input>
-                <input
-                  className="form__input"
-                  type="tel"
-                  name="tel"
-                  id="tel"
-                  autocomplete="tel"
-                  placeholder={t('form.phone')}
-                  required
-                ></input>
-                <input
-                  className="form__input"
-                  type="email"
-                  name="mail"
-                  id="mail"
-                  autocomplete="email"
-                  placeholder={t('form.email')}
-                  required
-                ></input>
-                <textarea
-                  name="comment"
-                  id="comment"
-                  rows="10"
-                  placeholder={t('form.comment')}
-                ></textarea>
-
-                <label>
+         
+          {/* {showModal && ( */}
+             <CSSTransition
+            in={showModal}
+            classNames="alert"
+            timeout={500}
+            unmountOnExit
+          >
+              <Modal onClose={toogleModal}>
+                <form className="form">
+                  <button
+                    className="btn__clouse hover form__icon"
+                    type="button"
+                    onClick={toogleModal}
+                  >
+                    X
+                  </button>
+                  <h1 className="form__title">
+                    {t('form.title')}
+                    {/* Leave your details, we will call you back */}
+                  </h1>
                   <input
-                    className="real__checkbox"
-                    type="checkbox"
-                    name="topic"
-                    value="js"
-                    id="js"
+                    className="form__input"
+                    type="text"
+                    name="name"
+                    id="name"
+                    autocomplete="name"
+                    placeholder={t('form.name')}
                     required
                   ></input>
-                  <span className="custom__checkbox"></span>
-                  <span className="form__text">
-                    {t('form.checkbox')}
-                    {/* I accept the terms and conditions of the */}
-                  </span>
-                  <a className="form__link" href="!#">
-                    <span>{t('form.span')}</span>
-                  </a>
-                </label>
-                <button className="btn" type="submit">
-                  {t('form.btn')}
-                </button>
-              </form>
-            </Modal>
-          )}
+                  <input
+                    className="form__input"
+                    type="tel"
+                    name="tel"
+                    id="tel"
+                    autocomplete="tel"
+                    placeholder={t('form.phone')}
+                    required
+                  ></input>
+                  <input
+                    className="form__input"
+                    type="email"
+                    name="mail"
+                    id="mail"
+                    autocomplete="email"
+                    placeholder={t('form.email')}
+                    required
+                  ></input>
+                  <textarea
+                    name="comment"
+                    id="comment"
+                    rows="10"
+                    placeholder={t('form.comment')}
+                  ></textarea>
+
+                  <label>
+                    <input
+                      className="real__checkbox"
+                      type="checkbox"
+                      name="topic"
+                      value="js"
+                      id="js"
+                      required
+                    ></input>
+                    <span className="custom__checkbox"></span>
+                    <span className="form__text">
+                      {t('form.checkbox')}
+                      {/* I accept the terms and conditions of the */}
+                    </span>
+                    <a className="form__link" href="!#">
+                      <span>{t('form.span')}</span>
+                    </a>
+                  </label>
+                  <button className="btn" type="submit">
+                    {t('form.btn')}
+                  </button>
+                </form>
+              </Modal>
+              </CSSTransition>
+            {/* )} */}
+          
         </div>
       </motion.section>
     </div>

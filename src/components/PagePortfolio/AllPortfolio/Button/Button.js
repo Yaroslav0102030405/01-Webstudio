f7b332a22384fa7047762d878b0c6d8836a4bfbc
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-10-25 19:22:37 +0300

    1

--- src/components/PagePortfolio/AllPortfolio/Button/Button.js
+++ src/components/PagePortfolio/AllPortfolio/Button/Button.js
@@ -1,20 +1,22 @@
 import React from 'react';
+// import portfolio from './../../../Translation/en/global.json'
+// import Filter from '../Filter/Filter';
 
 import { useTranslation } from 'react-i18next';
 
-const Button = ({ button, filter, title }) => {
+const Button = ({ button, filter, title}) => {
   const [t, i18n] = useTranslation('global');
+  // const [t, i18n] = useTranslation('filter');
   
   return (
     <>
       <ul className="allportfolio__list">
-        {button.map((description, id) => {
+        {button.map((description) => {
           return (
             <li className="allportfolio__item">
               <button type="button" onClick={() => filter(description)}>
                 {description}
-                {/* {t('portfolio.desc-1')} */}
-             
+                {/* {t('portfolio.btn')} */}
               </button>
             </li>
           );
--- src/components/PagePortfolio/AllPortfolio/Menu/Menu.js
+++ src/components/PagePortfolio/AllPortfolio/Menu/Menu.js
@@ -9,23 +9,24 @@ const Menu = ({ menuItem }) => {
   
     return (
       <>
-        <ul className="allportfolio__item2">
-          {menuItem.map(({ id, image, title, description }) => (
-            <li className="allportfolio__link" key={id}>
-              <div className="box__active">
-                <img
-                  loading="lazy"
-                  className="lazyload"
-                  src={image}
-                  alt={title}
-                ></img>
-                <div className="box__overlay">{/* </a> */}</div>
-              </div>
-              <h2>{title}</h2>
-              <p>{description}</p>
-            </li>
-          ))}
-        </ul>
+            <ul className="allportfolio__item2">
+              {menuItem.map(({ id, image, title, description }) => (
+                <li className="allportfolio__link" key={id}>
+                  <div className="box__active">
+                    <img
+                      loading="lazy"
+                      className="lazyload"
+                      src={image}
+                      alt={title}
+                    ></img>
+                    <div className="box__overlay">{/* </a> */}</div>
+                  </div>
+                  <h2>{title}</h2>
+                  <p>{description}</p>
+                </li>
+              ))}
+            </ul>
+
       </>
     );
 }

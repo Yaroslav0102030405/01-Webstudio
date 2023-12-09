48e152b93c49e35dec64d3e71d519de3876c5598
Yaroslav0102030405 <lukovetskyi@gmail.com>
2023-12-09 19:42:21 +0200

    update

--- src/HOME-PAGE/Hero/Hero.js
+++ src/HOME-PAGE/Hero/Hero.js
@@ -1,35 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { CSSTransition } from 'react-transition-group';
 // import { motion } from 'framer-motion';
 import './Hero.scss';
-// import Modal from '../../components/Modal/Modal';
+import Modal from '../../components/Modal/Modal';
 import './../../components/Modal/Modal.scss';
 
-// import { useTranslation } from 'react-i18next';
-import IndividualIntervalsExample from '../../components/BootstrapCarousels/BootsrapCarousels';
+import { useTranslation } from 'react-i18next';
+// import IndividualIntervalsExample from '../../components/BootstrapCarousels/BootsrapCarousels';
 
-const Animation = {
-  hidden: {
-    x: -100,
-    opacity: 0,
-  },
-  visible: custom => ({
-    x: 0,
-    opacity: 1,
-    transition: { delay: custom * 0.2 },
-  }),
-};
+// const Animation = {
+//   hidden: {
+//     x: -100,
+//     opacity: 0,
+//   },
+//   visible: custom => ({
+//     x: 0,
+//     opacity: 1,
+//     transition: { delay: custom * 0.2 },
+//   }),
+// };
 
 const Hero = () => {
-  // const [t, i18n] = useTranslation('global');
-  // const [showModal, setState] = useState(false);
-  // //  state = {
-  // //   showModal: false,
-  // // };
-
-  // const toogleModal = () => {
-  //   setState(!showModal);
+  const [t, i18n] = useTranslation('global');
+  const [showModal, setState] = useState(false);
+  //  state = {
+  //   showModal: false,
   // };
+
+  const toogleModal = () => {
+    setState(!showModal);
+  };
 
   // render() {
 
@@ -39,105 +39,98 @@ const Hero = () => {
 
   return (
     <>
-      <IndividualIntervalsExample />
-      {/* <div className="overlay"> */}
-      {/* <motion.section
-        initial="hidden"
-        whileInView="visible"
-        viewport={{ amount: 0.2, once: true }}
-        className="hero center"
-      > */}
-      <div className="container hero-container">
-        {/* <motion.h1 custom={1} variants={Animation} className="hero__title">
-            {t('hero.title')}
-          </motion.h1> */}
+      {/* <IndividualIntervalsExample /> */}
+      <div className="overlay">
+        <section className="hero center">
+          <div className="container hero-container">
+            <h1 className="hero__title">{t('hero.title')}</h1>
 
-        {/* <motion.button
-            custom={2}
-            variants={Animation}
-            className="btn"
-            type="button"
-            onClick={toogleModal}
-          >
-            {t('hero.btn')}
-          </motion.button> */}
+            <button
+              custom={2}
+              variants={Animation}
+              className="btn"
+              type="button"
+              onClick={toogleModal}
+            >
+              {t('hero.btn')}
+            </button>
 
-        {/* {showModal && ( */}
-        {/* <CSSTransition
-          in={showModal}
-          classNames="alert"
-          timeout={500}
-          unmountOnExit
-        >
-          <Modal onClose={toogleModal}>
-            <form className="form">
-              <button
-                className="btn__clouse hover form__icon"
-                type="button"
-                onClick={toogleModal}
-              >
-                x
-              </button>
-              <h1 className="form__title">{t('form.title')}</h1>
-              <input
-                className="form__input"
-                type="text"
-                name="name"
-                id="name"
-                autocomplete="name"
-                placeholder={t('form.name')}
-                required
-              ></input>
-              <input
-                className="form__input"
-                type="tel"
-                name="tel"
-                id="tel"
-                autocomplete="tel"
-                placeholder={t('form.phone')}
-                required
-              ></input>
-              <input
-                className="form__input"
-                type="email"
-                name="mail"
-                id="mail"
-                autocomplete="email"
-                placeholder={t('form.email')}
-                required
-              ></input>
-              <textarea
-                name="comment"
-                id="comment"
-                rows="10"
-                placeholder={t('form.comment')}
-              ></textarea>
+            {showModal && (
+              // <CSSTransition
+              //   in={showModal}
+              //   classNames="alert"
+              //   timeout={500}
+              //   unmountOnExit
+              // >
+              <Modal onClose={toogleModal}>
+                <form className="form">
+                  <button
+                    className="btn__clouse hover form__icon"
+                    type="button"
+                    onClick={toogleModal}
+                  >
+                    x
+                  </button>
+                  <h1 className="form__title">{t('form.title')}</h1>
+                  <input
+                    className="form__input"
+                    type="text"
+                    name="name"
+                    id="name"
+                    autocomplete="name"
+                    placeholder={t('form.name')}
+                    required
+                  ></input>
+                  <input
+                    className="form__input"
+                    type="tel"
+                    name="tel"
+                    id="tel"
+                    autocomplete="tel"
+                    placeholder={t('form.phone')}
+                    required
+                  ></input>
+                  <input
+                    className="form__input"
+                    type="email"
+                    name="mail"
+                    id="mail"
+                    autocomplete="email"
+                    placeholder={t('form.email')}
+                    required
+                  ></input>
+                  <textarea
+                    name="comment"
+                    id="comment"
+                    rows="10"
+                    placeholder={t('form.comment')}
+                  ></textarea>
 
-              <label>
-                <input
-                  className="real__checkbox"
-                  type="checkbox"
-                  name="topic"
-                  value="js"
-                  id="js"
-                  required
-                ></input>
-                <span className="custom__checkbox"></span>
-                <span className="form__text">{t('form.checkbox')}</span>
-                <a className="form__link" href="#">
-                  <span>{t('form.span')}</span>
-                </a>
-              </label>
-              <button className="btn" type="submit">
-                {t('form.btn')}
-              </button>
-            </form>
-          </Modal>
-        </CSSTransition> */}
-        {/* )} */}
+                  <label>
+                    <input
+                      className="real__checkbox"
+                      type="checkbox"
+                      name="topic"
+                      value="js"
+                      id="js"
+                      required
+                    ></input>
+                    <span className="custom__checkbox"></span>
+                    <span className="form__text">{t('form.checkbox')}</span>
+                    <a className="form__link" href="#">
+                      <span>{t('form.span')}</span>
+                    </a>
+                  </label>
+                  <button className="btn" type="submit">
+                    {t('form.btn')}
+                  </button>
+                </form>
+              </Modal>
+              // </CSSTransition>
+            )}
+          </div>
+        </section>
       </div>
-      {/* </motion.section> */}
-      {/* </div> */}
     </>
   );
 };
--- src/components/BootstrapCarousels/BootsrapCarousels.js
+++ src/components/BootstrapCarousels/BootsrapCarousels.js
@@ -1,118 +1,118 @@
-import React from 'react';
-import Modal from '../Modal/Modal';
-import './../../HOME-PAGE/Hero/Hero.scss';
-import './BootstrapCarousels.scss';
+// import React from 'react';
+// import Modal from '../Modal/Modal';
+// import './../../HOME-PAGE/Hero/Hero.scss';
+// import './BootstrapCarousels.scss';
 
-import Button from '../Button/Button';
+// import Button from '../Button/Button';
 
-import Carousel from 'react-bootstrap/Carousel';
-// import ExampleCarouselImage from 'components/ExampleCarouselImage';
-// import Desctop from '../../images/hero/desctopFirst-1x.png';
-// import Desctop2 from '../../images/hero/desctop-2--1x.jpg';
-// import Desctop3 from '../../images/hero/desctop-3--1x.jpg';
+// import Carousel from 'react-bootstrap/Carousel';
+// // import ExampleCarouselImage from 'components/ExampleCarouselImage';
+// // import Desctop from '../../images/hero/desctopFirst-1x.png';
+// // import Desctop2 from '../../images/hero/desctop-2--1x.jpg';
+// // import Desctop3 from '../../images/hero/desctop-3--1x.jpg';
 
-import Mobile from '../../images/hero/bh-img-480-2x.jpg';
-// import Mobile2 from '../../images/hero/Mobile-2-480-2x.jpg';
-// import Mobile3 from '../../images/hero/Mobile-3-480-2x.jpg';
+// import Mobile from '../../images/hero/bh-img-480-2x.jpg';
+// // import Mobile2 from '../../images/hero/Mobile-2-480-2x.jpg';
+// // import Mobile3 from '../../images/hero/Mobile-3-480-2x.jpg';
 
-import Tablet from '../../images/hero/bg-img-768-2x.jpg';
-// import Tablet2 from '../../images/hero/tablet-2-768-2x.jpg';
-// import Tablet3 from '../../images/hero/tablet-3-768-2x.jpg';
+// import Tablet from '../../images/hero/bg-img-768-2x.jpg';
+// // import Tablet2 from '../../images/hero/tablet-2-768-2x.jpg';
+// // import Tablet3 from '../../images/hero/tablet-3-768-2x.jpg';
 
-import DesctopWebP1 from '../../images/hero/WebP/desctopFirst-1x.WebP';
-import DesctopWebP2 from '../../images/hero/WebP/desctop-2--1x.WebP';
-import DesctopWebP3 from '../../images/hero/WebP/desctop-3--1x.WebP';
+// import DesctopWebP1 from '../../images/hero/WebP/desctopFirst-1x.WebP';
+// import DesctopWebP2 from '../../images/hero/WebP/desctop-2--1x.WebP';
+// import DesctopWebP3 from '../../images/hero/WebP/desctop-3--1x.WebP';
 
-import TabletWebP2 from '../../images/hero/WebP/tablet-2-768-2x.WebP';
-import TabletWebP3 from '../../images/hero/WebP/tablet-3-768-2x.WebP';
+// import TabletWebP2 from '../../images/hero/WebP/tablet-2-768-2x.WebP';
+// import TabletWebP3 from '../../images/hero/WebP/tablet-3-768-2x.WebP';
 
-import MobileWebP2 from '../../images/hero/WebP/Mobile-2-480-2x.WebP';
-import MobileWebP3 from '../../images/hero/WebP/Mobile-3-480-2x.WebP';
+// import MobileWebP2 from '../../images/hero/WebP/Mobile-2-480-2x.WebP';
+// import MobileWebP3 from '../../images/hero/WebP/Mobile-3-480-2x.WebP';
 
-function IndividualIntervalsExample() {
-  return (
-    <Carousel data-bs-theme="light">
-      <Carousel.Item interval={1000}>
-        <picture>
-          <source srcSet={Mobile} media="(max-width: 767px)" type="image/jpg" />
-          <source
-            srcSet={Tablet}
-            media="(min-width: 768px) and (max-width: 1199px)"
-          />
-          <source srcSet={DesctopWebP1} media="(min-width: 1200px)" />
-          <img
-            width="100%"
-            height="auto"
-            className="d-block w-100 Desctop"
-            src={DesctopWebP1}
-            alt="First slide"
-          />
-        </picture>
-        <Carousel.Caption>
-          {/* <h1 className="hero__title">Effective Solutions for Your Business</h1> */}
-          {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
-          {/* <button className="btn" type="button">
-            Order Service
-          </button> */}
-          <Button />
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <picture>
-          <source
-            srcSet={MobileWebP2}
-            media="(max-width: 767px)"
-            type="image/jpg"
-          />
-          <source
-            srcSet={TabletWebP2}
-            media="(min-width: 768px) and (max-width: 1199px)"
-          />
-          <source srcSet={DesctopWebP2} media="(min-width: 1200px)" />
-          <img
-            width="100%"
-            height="auto"
-            className="d-block w-100 Backround__image"
-            src={DesctopWebP2}
-            alt="Second slide"
-          />
-        </picture>
-        <Carousel.Caption>
-          {/* <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
-          <Button />
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={1000}>
-        <picture>
-          <source
-            srcSet={MobileWebP3}
-            media="(max-width: 767px)"
-            type="image/jpg"
-          />
-          <source
-            srcSet={TabletWebP3}
-            media="(min-width: 768px) and (max-width: 1199px)"
-          />
-          <source srcSet={DesctopWebP3} media="(min-width: 1200px)" />
-          <img
-            width="100%"
-            height="auto"
-            className="d-block w-100"
-            src={DesctopWebP3}
-            alt="Third slide"
-          />
-        </picture>
-        <Carousel.Caption>
-          {/* <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p> */}
-          <Button />
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
-}
+// function IndividualIntervalsExample() {
+//   return (
+//     <Carousel data-bs-theme="light">
+//       <Carousel.Item interval={1000}>
+//         <picture>
+//           <source srcSet={Mobile} media="(max-width: 767px)" type="image/jpg" />
+//           <source
+//             srcSet={Tablet}
+//             media="(min-width: 768px) and (max-width: 1199px)"
+//           />
+//           <source srcSet={DesctopWebP1} media="(min-width: 1200px)" />
+//           <img
+//             width="100%"
+//             height="auto"
+//             className="d-block w-100 Desctop"
+//             src={DesctopWebP1}
+//             alt="First slide"
+//           />
+//         </picture>
+//         <Carousel.Caption>
+//           {/* <h1 className="hero__title">Effective Solutions for Your Business</h1> */}
+//           {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+//           {/* <button className="btn" type="button">
+//             Order Service
+//           </button> */}
+//           <Button />
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item interval={1000}>
+//         <picture>
+//           <source
+//             srcSet={MobileWebP2}
+//             media="(max-width: 767px)"
+//             type="image/jpg"
+//           />
+//           <source
+//             srcSet={TabletWebP2}
+//             media="(min-width: 768px) and (max-width: 1199px)"
+//           />
+//           <source srcSet={DesctopWebP2} media="(min-width: 1200px)" />
+//           <img
+//             width="100%"
+//             height="auto"
+//             className="d-block w-100 Backround__image"
+//             src={DesctopWebP2}
+//             alt="Second slide"
+//           />
+//         </picture>
+//         <Carousel.Caption>
+//           {/* <h5>Second slide label</h5>
+//           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+//           <Button />
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//       <Carousel.Item interval={1000}>
+//         <picture>
+//           <source
+//             srcSet={MobileWebP3}
+//             media="(max-width: 767px)"
+//             type="image/jpg"
+//           />
+//           <source
+//             srcSet={TabletWebP3}
+//             media="(min-width: 768px) and (max-width: 1199px)"
+//           />
+//           <source srcSet={DesctopWebP3} media="(min-width: 1200px)" />
+//           <img
+//             width="100%"
+//             height="auto"
+//             className="d-block w-100"
+//             src={DesctopWebP3}
+//             alt="Third slide"
+//           />
+//         </picture>
+//         <Carousel.Caption>
+//           {/* <h5>Third slide label</h5>
+//           <p>
+//             Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+//           </p> */}
+//           <Button />
+//         </Carousel.Caption>
+//       </Carousel.Item>
+//     </Carousel>
+//   );
+// }
 
-export default IndividualIntervalsExample;
+// export default IndividualIntervalsExample;
